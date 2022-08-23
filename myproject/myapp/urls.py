7c9ab33686d438434af97a803b06ace3e9965a04
nayeon0000 <wqazx8517@gmail.com>
2022-08-23 17:54:37 +0900

    drf2

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
     path('<int:movie_pk>/', views.movie_detail_update_delete),
     path('<int:movie_pk>/reviews/', views.review_list_create),
     path('<int:movie_pk>/reviews/<int:review_pk>', views.review_detail_update_delete),
-    path('regist/', user_regist, name = 'regist'),
-    path('login/', user_login, name = 'login'),
-    path('test/', test, name = 'test'),
+    path('regist', user_regist, name = 'regist'),
+    path('login', user_login, name = 'login'),
+    path('test', test, name = 'test'),
 ]
